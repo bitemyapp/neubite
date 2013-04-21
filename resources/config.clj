e75a6263c9aa324ea6e08090e3809f9000adcac0
Chris Allen <cma@bitemyapp.com>
2013-04-20 22:31:13 -0700

    ssl nil, no carica

--- resources/config.clj
+++
@@ -1,5 +0,0 @@
-{:dburi (get (System/getenv) "DATABASE_URL")
- :secret (get (System/getenv) "SECRET_KEY")
- :ssl (get (System/getenv) "SSL")
- :admins ["cma@bitemyapp.com"]
-}
--- src/neubite/config.clj
+++ src/neubite/config.clj
@@ -2,5 +2,6 @@
 
 (def config {:dburi (get (System/getenv) "DATABASE_URL")
              :secret (get (System/getenv) "SECRET_KEY")
-             :ssl (get (System/getenv) "SSL")
+             ; :ssl (get (System/getenv) "SSL")
+             :ssl nil
              :admins ["cma@bitemyapp.com"]})
